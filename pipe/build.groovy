5f1722bccbc169904a58b59e1e59781c7fd4d57b
kalabar <errer8953@gmail.com>
2025-04-16 13:38:36 +0300

    update

--- pipe/build.groovy
+++ pipe/build.groovy
@@ -38,6 +38,7 @@ pipeline {
                         sh """
                             docker build -t ${image} .
                             docker push ${image}
+                            docker logout
                         """    
                     }
                 }
